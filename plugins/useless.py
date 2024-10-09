47164127f1ad1873bc0c5caacced9ea6fcecf680
rohit8708913 <koyeb67user@gmail.com>
2024-10-09 20:49:49 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -13,4 +13,9 @@ async def stats(bot: Bot, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
+@Bot.on_message(filters.private & filters.incoming)
+async def useless(_,message: Message):
+    if USER_REPLY_TEXT:
+        await message.reply(USER_REPLY_TEXT)
+
 

837c7b5f9389091fabeef77eaa57aa305bc93c50
rohit8708913 <koyeb67user@gmail.com>
2024-10-09 20:14:06 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,3 +1,5 @@
+# Don't remove This Line From Here. Tg: @im_piro | @PiroHackz
+
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
@@ -33,10 +35,15 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    link = f"https://telegram.me/{client.username}?start={base64_string}"
 
+    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
+    string = string.replace("get-", "premium-")
+    base64_string = await encode(string)
+    link1 = f"https://telegram.me/{client.username}?start={base64_string}"
+
+    reply_markup = InlineKeyboardMarkup([ [InlineKeyboardButton("Public Link", url=f"{link}")], [InlineKeyboardButton("Premium Link", url=f"{link1}")]])
+    await second_message.reply_text(f"<b>Here are your links.</b>", quote=True, reply_markup=reply_markup)
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
@@ -53,6 +60,8 @@ async def link_generator(client: Client, message: Message):
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    link = f"https://telegram.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+
+# Don't remove This Line From Here. Tg: @im_piro | @PiroHackz
